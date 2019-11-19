ef4e590c161b9ca3a253ba319305d0f8883edcda
r554 <57042721+r554@users.noreply.github.com>
2019-11-19 11:16:29 +0700

    akun terdaftar admin
    akun terdaftar admin

---
+++ views/admin/akun_terdaftar.php
@@ -0,0 +1,71 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="../../assets/css/bootstrap.css">
+</head>
+
+</html>
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Daftar Account Developer</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="?page=pendaftar">Home</a></li>
+                    <li class="breadcrumb-item active">Akun Terdaftar</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+
+
+    <!-- Default box -->
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Title</h3>
+
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fas fa-minus"></i></button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                    <i class="fas fa-times"></i></button>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <form action="" method="POST">
+                <table border="1" style="text-align:center" width=1000>
+                    <tr>
+                        <th>NIK</th>
+                        <th>Nama Developer</th>
+                        <th>Alamat Developer</th>
+                        <th>No Telp</th>
+                        <th>Username Developer</th>
+                        <th>Password Developer</th>
+                        <th colspan="2">Aksi</th>
+                    </tr>
+                    <tr>
+                        <td>1234567891234567</td>
+                        <td>Agus</td>
+                        <td>Alamat Developer</td>
+                        <td>08554321452</td>
+                        <td>root</td>
+                        <td>1234</td>
+                        <td><input type="submit" name="submit" value="edit" class="btn btn-info"></td>
+                        <td><input type="submit" name="submit" value="hapus" class="btn btn-danger"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+            Footer
+        </div>
+        <!-- /.card-footer-->
+    </div>
+    <!-- /.card -->
+
+
+</section>
