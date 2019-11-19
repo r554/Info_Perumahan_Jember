b03072b764d53953a33ab36bb91d3936b7466845
r554 <57042721+r554@users.noreply.github.com>
2019-11-19 11:14:41 +0700

    pendaftar admin
    pendaftar admin

---
+++ views/admin/pendaftar.php
@@ -0,0 +1,15 @@
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Pendaftar</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="?page=pendaftar">Home</a></li>
+                    <li class="breadcrumb-item active">Pendaftar</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
