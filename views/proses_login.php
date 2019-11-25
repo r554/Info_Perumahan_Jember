80020231c34d0cbd26e6f629ca9f40b50dacd8d6
muhrezhahaironi <56098802+muhrezhahaironi@users.noreply.github.com>
2019-11-25 09:52:44 +0700

    prosesogindeveloper
    proseslogindeveloper

--- views/proses_login.php
+++ views/proses_login.php
@@ -15,15 +15,17 @@ $password = $_POST['password'];
 
 // menyeleksi data admin dengan username dan password yang sesuai
 $data = mysqli_query($koneksi, "select * from tabel_developer where username_dev='$username' and password_dev='$password'");
-
+$row = mysqli_fetch_array($data);
 // menghitung jumlah data yang ditemukan
 $cek = mysqli_num_rows($data);
 
 if ($cek > 0) {
+    session_start();
     $_SESSION['username_dev'] = $username;
+    $_SESSION['id'] = $row['nik'];
     $_SESSION['status'] = "login";
     echo $_SESSION['username_dev'];
-    header("location:admin/index.php");
+    header("location:developer/index.php");
 } else {
     header("location:index.php?pesan=gagal");
 }
