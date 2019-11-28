e68008b449f1ff60570931156ec45f123baa25dd
harismansihombing <sihombingharisman28@gmail.com>
2019-11-28 07:28:25 +0700

    Update Postingan.php

--- views/developer/Postingan.php
+++ views/developer/Postingan.php
@@ -5,12 +5,128 @@
   <title>form</title>
 </head>
 <body>
+
+
+
+<nav class="navbar navbar-dark bg-dark">
+
+<div class="dropright">
+  <button class="btn btn-outline-info dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    MENU
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="?page=postingan">Riwayat Postingan</a>
+        <a class="dropdown-item" href="?page=tambahpostingan">Tambah Postingan</a>
+        <a class="dropdown-item" href="?page=editdatarumah">Edit Postingan</a>
+  </div> 
+</div>
+
+
+<h5 class="text-center"> RIWAYAT POSTINGAN</h5>
+<h5 class="text-center">
+
+
+<div class="dropleft">
+  <button class="btn btn-outline-light btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Info!
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="?page=postingan">Bagian ini berisi riwayat postingan <br> yang pernah anda lakukan.</a>
+  </div> 
+</div>
+
+
+
+</h5>
+
+</nav> 
+
+
+<br>
+<table class="table table-hover">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Kode Data Rumah</th>
+      <th scope="col">Tipe Rumah</th>
+      <th scope="col">Harga</th>
+      <th scope="col">Jumlah Unit</th>
+      <th scope="col">Status</th>
+      <th scope="col"></th>
+    </tr>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>ABC1</td>
+      <td>34</td>
+      <td>Rp.230.000.00</td>
+      <td>30</td>
+      <td>Tersedia</td>
+      <td><a href="#" class="btn btn-outline-info btn-sm" role="button"> LIHAT</a>
+      </td>
+    </tr>
+
+    <tr>
+      <th scope="row">2</th>
+      <td>ABC2</td>
+      <td>45</td>
+      <td>Rp.330.000.00</td>
+      <td>10</td>
+      <td>Tersedia</td>
+      <td><a href="#" class="btn btn-outline-info btn-sm" role="button"> LIHAT</a>
+      </td>
+    </tr>
+
+    <tr>
+      <th scope="row">3</th>
+      <td>ABC3</td>
+      <td>55</td>
+      <td>Rp.430.000.00</td>
+      <td>5</td>
+      <td>Terjual</td>
+      <td><a href="#" class="btn btn-outline-info btn-sm" role="button"> LIHAT</a>
+      </td>
+    </tr>
+
+    <tr>
+      <th scope="row">4</th>
+      <td>ABC4</td>
+      <td>24</td>
+      <td>Rp.130.000.00</td>
+      <td>43</td>
+      <td>Tersedia</td>
+      <td><a href="#" class="btn btn-outline-info btn-sm" role="button"> LIHAT</a>
+      </td>
+    </tr>
+
+    <tr>
+      <th scope="row">5</th>
+      <td>ABC5</td>
+      <td>75</td>
+      <td>Rp.630.000.00</td>
+      <td>10</td>
+      <td>Tersedia</td>
+      <td><a href="#" class="btn btn-outline-info btn-sm" role="button"> LIHAT</a>
+      </td>
+    </tr>
+
+
+
+   
+  </tbody>
+</table>
+
+
+
+
+
+
         <!-- 
                 <h3>Tambah Data Rumah</h3>
 
     <form method="POST" action="proses.php">
-
-
     
         Kode Data Rumah <input type="text"  name="kd_data_rumah"
         placeholder="isikan kode"> 
@@ -86,23 +202,41 @@
 
 
 
-<div class="container-fluid">
-<h1>Tambah Data Rumah</h1>
 
-<form method="POST" action="proses.php">
+
+
+
+
+
+                                                        
+<!--
+<div class="text-bold">
+<div class="text-center">
+<div class="p-3 mb-2 bg-dark text-warning">Tambah Data Rumah</div>
+ </div>
+ </div>
+ 
+ 
+ 
+<div class="container-fluid">
+<form  method="POST" action="prosespost.php">
 
         <div class="row">
         <div class="col-sm-4">
         <div class="col">
-        Kode Data Rumah <input type="text" class="form-control" name="kd_data_rumah" placeholder="Kode Rumah">
+        Kode Data Rumah<input type="text" class="form-control" name="kd_data_rumah" placeholder="Kode Rumah">
         </div>
         </div>
+        
                 <div class="col-sm-4">
                 <div class="col">
-                Luas Tanah <input type="number" class="form-control" name="luas_tanah" placeholder="Luas Tanah">
+                Judul Postingan <input type="text" class="form-control" name="judul_postingan" placeholder="Judul Postingan">
                 </div>
                 </div>
                 </div>
+
+              
+
 
         <div class="row">
         <div class="col-sm-4">
@@ -179,12 +313,16 @@
                 </div>
                 </div>
                 </div>
+
+    
+
         <div class="row">
-        <div class="form-group col-md-4"> 
+        <div class="form-group col-md-4">        
         <div class="col">
-        Status Rumah <input type="text" class="form-control" name="status" placeholder="Status">
+        Tutup Plafon <input type="text" class="form-control" name="tutup_plafon" placeholder="Tutup Plafon">
         </div>
         </div>
+        
                 <div class="col-sm-4"> 
                 <div class="col">
               Kusen <input type="text" class="form-control" name="kusen" placeholder="Kusen">
@@ -206,7 +344,7 @@
         <div class="row">
         <div class="form-group col-md-4">        
         <div class="col">
-        Kerangka Atap <input type="text" class="form-control" name="kerangkat_atap" placeholder="Kerangka Atap">
+        Kerangka Atap <input type="text" class="form-control" name="kerangka_atap" placeholder="Kerangka Atap">
         </div>
         </div>
                 <div class="col-sm-4">     
@@ -216,52 +354,65 @@
                 </div>
                 </div>
 
-        <div class="row">
-        <div class="form-group col-md-4">        
-        <div class="col">
-        Tutup Plafon <input type="text" class="form-control" name="tutup_plafon" placeholder="Tutup Plafon">
-        </div>
-        </div>
-        
-                <div class="col-sm-4">     
-                <div class="col"> 
-                Masukan Link Google Maps Perumahan <input type="text" class="form-control-sm" name="latitude" placeholder="latitude">
-                <input type="text" class="form-control-sm" name="longitude" placeholder="longitude">
-                </div>
-                </div>
-                </div>
-                
-               
 
+
+                <div class="row">
+                <div class="form-group col-md-4">
+      <label for="inputState">Status Rumah</label>
+      <select id="inputState" class="form-control">
+        <option selected>Tersedia</option>
+        <option> Tidak Tersedia</option>
+      </select>
+    </div>
+
+    <div class="col-sm-4">
+                <div class="col">
+                Luas Tanah <input type="number" class="form-control" name="luas_tanah" placeholder="Luas Tanah">
+                </div>
+                </div>
+                </div>
+
+                
+    
+                <br>
+<div class="container-fluid">
     <div class="row">
     <div class="form-group col-md-4">        
     <div class="col">
-        Upload Foto Rumah <input type="file" class="form-control" name="foto_1">
+       <h6 class="text-center"> UPLOAD FOTO RUMAH </h6> <input type="file" class="form-control" name="foto_1"> 
         Foto Kamar<input type="file" class="form-control" name="foto_2">
         Foto Kamar Mandi<input type="file" class="form-control" name="foto_3">
         Foto Ruang Tamu<input type="file" class="form-control" name="foto_4" >
         Foto Dapur<input type="file" class="form-control" name="foto_5">
 </div>
 </div>
+<div class="col-sm-4">     
+                <div class="col"> 
+               <h6 class="text-center"> Masukan Link Google Maps Perumahan <input type="text" class="form-control-sm" name="latitude" placeholder="latitude">
+                <input type="text" class="form-control-sm" name="longitude" placeholder="longitude">
+                </div>
+                </div>
+                </div>
 </div>
 
 
 
 
 
-</form>
-
-
-
-
-
-        <p>
+<p>
         <div class="form-group col-md-4"> 
             <input type="submit"name="btn_Submit"
             value="submit" class="btn btn-secondary">
         </div>
         </p>
 
-        
+</form>
+
+
+
+-->
+
+
+     
 </body>
 </html>
